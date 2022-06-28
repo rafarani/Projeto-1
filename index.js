11b10d469a038f116eed9fe4945a7357d1e278b4
Rafael Rani <ranny008@hotmail.com>
2022-06-28 13:16:13 -0300

    segundo commit

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ var array = [2, 5, 7, 8, 9]
 //rodar 4 vezes
 var aux = array[0];
 for(let i=0; i<array.length-1; i++){
-    console.log(array[i])
+    array[i] = array[i+1]
 }
+array[array.length-1] = aux
+console.log(array)
