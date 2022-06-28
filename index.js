664f21a7f11720c9daa008aec78ac3a340e70c80
Rafael Rani <ranny008@hotmail.com>
2022-06-28 13:21:34 -0300

    terceiro deploy

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var array = [2, 5, 7, 8, 9]
+var array = [1, 2, 3, 4, 5]
+var contador = 4;
 
-//rodar 4 vezes
-var aux = array[0];
-for(let i=0; i<array.length-1; i++){
-    array[i] = array[i+1]
+while(contador > 0){
+    var aux = array[0];
+    for(let i=0; i<array.length-1; i++){
+        array[i] = array[i+1];
+    }
+    array[array.length-1] = aux;
+    console.log(array);
+    contador--;
 }
-array[array.length-1] = aux
-console.log(array)
